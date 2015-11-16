34efe19fea385a2598480976bf87e6da043a76ab
vthai <veng.thai@gmail.com>
2015-11-16 21:59:51 +1000

    add in the process.nextTick and setImmediatTimeout test

---
+++ nodeAsyncTimeout.js
@@ -0,0 +1,25 @@
+var http = require('http');
+var url = require('url');
+
+http.createServer(function (req, res) {
+    var path = req.url;
+    var param = url.parse(path, true);
+    var time = param['query']['task'];
+    console.log('Serving ' + path + ' START', time);
+
+    setImmediate(function() {
+        console.log('Serving TO ' + path + ' \tEXE START');
+        var sum = 0;
+        for (i = 0; i < time; i++) {
+            sum = sum + i;
+        }
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Hello World timeout ' + path + '->' + sum);
+
+        console.log('Serving TO ' + path + ' \tEXE END');
+    });
+    
+    console.log('Serving ' + path + ' END');
+}).listen(1337, "127.0.0.1");
+
+console.log('Server running at http://127.0.0.1:1337/');
